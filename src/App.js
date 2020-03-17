25f5509f56cf2f5613e8c57af8100cd586b93c11
Shrikant <itzshrikant@gmail.com>
2020-03-17 23:17:09 +0530

    routing added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+
 import './App.css';
+
 import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <h1>HATS PAGE </h1>
+    </div>
+  )
+};
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop/hats' component={HatsPage} />
+        </Switch>
+      </BrowserRouter>
+
     </div>
   );
 }
